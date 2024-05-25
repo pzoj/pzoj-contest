df275c4ce39f452ae420a2da912d48d747f23731
Kevin Lu <kevie.lu8@gmail.com>
2024-05-25 09:31:17 -0700

    update todo; remove contests

--- web-backend/index.js
+++ web-backend/index.js
@@ -190,12 +190,6 @@ app.get('/api/problem/:pid', (req, res) => {
 		res.send('Problem not found');
 		return;
 	}
-	data = data.toString().split('\n');
-	if (data[3] == '0') {
-		res.status(403);
-		res.send('Problem is private');
-		return;
-	}
 	fs.readFile(path.join(problem_path, 'problem.md'), (err, data) => {
 		if (err) {
 			console.error(err);
@@ -294,14 +288,12 @@ app.get('/api/problems', (req, res) => {
 			];
 			if (req.query.q && !tmp[0].includes(req.query.q) && !tmp[1].includes(req.query.q))
 				return;
-			if (tmp[4] != '0') {
-				problems.push({
-					pid: tmp[0],
-					title: tmp[1],
-					difficulty: tmp[2],
-					tag: tmp[3],
-				});
-			}
+			problems.push({
+				pid: tmp[0],
+				title: tmp[1],
+				difficulty: tmp[2],
+				tag: tmp[3],
+			});
 		});
 		res.send(JSON.stringify(problems));
 	});
@@ -356,114 +348,6 @@ wss.on('connection', (ws) => {
 			});
 		});
 	});
-});
-
-/* ------------------ CONTESTS ------------------ */
-
-var contests = [];
-
-app.get('/api/contests/:cid/', (req, res) => {
-	// send contest statement
-	let contest_path = path.join(cwd(), '..', 'contests', req.params.cid);
-	fs.readFile(path.join(contest_path, 'statement.md'), (err, data) => {
-		if (err) {
-			console.error(err);
-			res.status(404);
-			res.send('Contest not found');
-			return;
-		}
-		data = fs.readFileSync(path.join(contest_path, 'statement.md'));
-		res.send(converter.makeHtml(data.toString()));
-	});
-});
-
-app.get('/api/contests/:cid/meta', (req, res) => {
-	// ../contests/:cid/meta.txt
-	let contest_path = path.join(cwd(), '..', 'contests', req.params.cid);
-	fs.readFile(path.join(contest_path, 'meta.txt'), (err, data) => {
-		if (err) {
-			console.error(err);
-			res.status(404);
-			res.send('Contest not found');
-			return;
-		}
-		data = data.toString().split('\n');
-		res.send({
-			cid: req.params.cid,
-			title: data[0],
-			start: data[1],
-			end: data[2],
-			rated: data[3] == '1'
-		});
-	});
-});
-
-app.get('/api/contests/:cid/problems', (req, res) => {
-	let contest_path = path.join(cwd(), '..', 'contests', req.params.cid, 'problems.txt');
-	let problems = [];
-	fs.readFile(contest_path, (err, data) => {
-		if (err) {
-			console.error(err);
-			res.status(404);
-			res.send('Contest not found');
-			return;
-		}
-		data = data.toString().split('\n');
-		data.forEach((line) => {
-			if (line == '') return;
-			problems.push({line});
-		});
-		res.send(problems);
-	});
-});
-
-app.get('/api/contests/:cid/leaderboard', (req, res) => {
-	let users = new Map();
-	let problems = [];
-	let contest_path = path.join(cwd(), '..', 'contests', req.params.cid);
-	fs.readFile(path.join(contest_path, 'problems.txt'), (err, data) => {
-		if (err) {
-			console.error(err);
-			res.status(404);
-			res.send('Contest not found');
-			return;
-		}
-		data = data.toString().split('\n');
-		data.forEach((line) => {
-			if (line == '') return;
-			problems.push({line});
-		});
-		for (let i = 0; i < problems.length; i++) {
-			db.all('SELECT * FROM submissions WHERE problemid=? AND result="AC"', [problems[i]], (err, rows) => {
-				if (err) {
-					console.error(err);
-					res.status(500);
-					res.end();
-					return;
-				}
-				rows.forEach((row) => {
-					if (!users.has(row.username)) {
-						users.set(row.username, {score: 0, solved: []});
-					}
-					if (user.solved[user.solved.length-1] != problems[i]) {
-						let user = users.get(row.username);
-						user.score += 1;
-						user.solved.push(problems[i]);
-						users.set(row.username, user);
-					}
-				});
-			});
-		}
-	});
-	// return users as an array sorted by score
-	let ret = [];
-	users.forEach((value, key) => {
-		ret.push({username: key, score: value.score, solved: value.solved});
-	});
-	ret.sort((a, b) => {
-		return b.score - a.score;
-	});
-	res.send(ret);
 });
 
 /* ------------------ LISTENING ------------------ */
