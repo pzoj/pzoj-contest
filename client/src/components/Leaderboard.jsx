9dbc35f5398509ad06e9f7d2a31cf75f15c40dd2
Kevin Lu <kevie.lu8@gmail.com>
2024-08-07 17:19:14 -0600

    touch up backend; add live leaderboard; add countdown

--- client/src/components/Leaderboard.jsx
+++ client/src/components/Leaderboard.jsx
@@ -13,7 +13,11 @@ const Leaderboard = ({ users }) => {
 					<tr>
 						<th className="border border-stone-400 p-3">Rank</th>
 						<th className='border border-stone-400 p-3'>Username</th>
-						{Object.keys(users[0].problems).map((key, index) => (
+						{/* {Object.keys(users[0].problems).map((key, index) => (
+							<th className='border border-stone-400 p-3' key={index}>{key}</th>
+						))} */}
+						{/*sort the problems based on problems[i].difficulty*/}
+						{Object.keys(users[0].problems).sort((a, b) => users[0].problems[a].difficulty - users[0].problems[b].difficulty).map((key, index) => (
 							<th className='border border-stone-400 p-3' key={index}>{key}</th>
 						))}
 						<th className='border border-stone-400 p-3'>Total Score</th>
@@ -25,9 +29,15 @@ const Leaderboard = ({ users }) => {
 						<tr key={user.username}>
 							<td className='border border-stone-400 p-3'>{rank++}</td>
 							<td className='border border-stone-400 p-3'>{user.username}</td>
-							{Object.keys(user.problems).map((key, index) => (
+							{/* {Object.keys(user.problems).map((key, index) => (
 								<td className={`border border-stone-400 p-3 ${user.problems[key].score==100?"text-green-500":"text-red-500"} text-center`} key={index}>
 									{user.problems[key].score?user.problems[key].score:""} {user.problems[key].score?`(${user.problems[key].penalty})`:""}
+								</td>
+							))} */}
+							{/*sort the problems based on problems[i].difficulty*/}
+							{Object.keys(user.problems).sort((a, b) => user.problems[a].difficulty - user.problems[b].difficulty).map((key, index) => (
+								<td className={`border border-stone-400 p-3 ${user.problems[key].score==100?"text-green-500":"text-red-500"} text-center`} key={index}>
+									{user.problems[key].penalty?user.problems[key].score:""} {user.problems[key].penalty?`(${user.problems[key].penalty})`:""}
 								</td>
 							))}
 							<td className='border border-stone-400 p-3 text-center'>{user.points}</td>
--- client/src/pages/liveleaderboard.jsx
+++ client/src/pages/liveleaderboard.jsx
@@ -12,6 +12,7 @@ import Leaderboard from "@/components/Leaderboard";
 
 function formatDate(time) {
 	// return in format HH:MM:SS
+	if (time < 0) return "00:00:00";
 	let hours = Math.floor(time / 3600);
 	time %= 3600;
 	let minutes = Math.floor(time / 60);
@@ -24,30 +25,34 @@ function formatDate(time) {
 }
 
 export default () => {
-	const [contestDescription, setContestDescription] = useState("");
 	const [endTime, setEndTime] = useState(0);
 	const [curTime, setCurTime] = useState(Date.now()/1000);
 	const [leaderboard, setLeaderboard] = useState([]);
 	const [isClient, setIsClient] = useState(false);
 	useEffect(() => {
-		axios.get("/api/contest").then(async (res) => {
-			setContestDescription(res.data);
-		}).catch((err) => {
-			console.error(err);
-		});
 		axios.get("/api/contest/meta").then(async (res) => {
 			setEndTime(parseInt(res.data.end));
 		});
 		const interval = setInterval(() => {
 			setCurTime(Date.now()/1000);
 		}, 1000);
+		const interval2 = setInterval(() => {
+			axios.get("/api/contest/leaderboard").then(async (res) => {
+				setLeaderboard(res.data);
+			}).catch((err) => {
+				console.error(err);
+			});
+		}, 5000);
 		axios.get("/api/contest/leaderboard").then(async (res) => {
 			setLeaderboard(res.data);
 		}).catch((err) => {
 			console.error(err);
 		});
 		setIsClient(true);
-		return () => clearInterval(interval);
+		return () => {
+			clearInterval(interval);
+			clearInterval(interval2);
+		}
 	}, []);
 
 	return (
@@ -64,15 +69,7 @@ export default () => {
 				]}
 			/>
 
-			<main className="bg-dark-0 w-full">
-				<div className="flex flex-row justify-center items-start relative w-4/5 mx-auto mt-[10rem] mb-[6rem]">
-					<div className="grow px-10">
-						<div id="problem-statement">
-							<InlineTex texSeperator="${1}" texContent={contestDescription} />
-						</div>
-					</div>
-				</div>
-			</main>
+			<div className="w-4/5 mx-auto mt-[10rem] mb-[6rem]" />
 
 			<div className="flex justify-center mb-[2rem] text-transparent bg-clip-text bg-gradient-to-br from-emerald-500 to-sky-500 font-bold text-4xl">
 				{/* <h1>Time Left: {formatDate(endTime - (Date.now()/1000))}</h1> */}
